2656f31923b23c74592e2364d131dff9f0ad4448
chrischen86 <chrischen86@gmail.com>
2017-06-18 03:14:52 -0400

    Update webhook

--- git-hook.php
+++ git-hook.php
@@ -1,12 +1,31 @@
 <?php
+
 echo "Begin: Pull code from GitHub<br/>";
 
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE); //convert JSON into array
 
-error_log(print_r($input, 1));
-exec('git pull', $output);
-foreach ($output as $o) {
-    echo $o . '<br/>';
+$array = preg_split('/\//', $input['ref']);
+$branch = array_values(array_slice($array, -1))[0];
+
+exec('git fetch', $output);
+PrintOutput($output);
+
+if ($branch != 'master')
+{
+    exec("git checkout $branch -f", $output);
+    PrintOutput($output);
 }
+
+exec('git pull', $output);
+PrintOutput($output);
+
 echo "End: Pull code from Github<br/>";
+
+function PrintOutput($output)
+{
+    foreach ($output as $o)
+    {
+        echo $o . '<br/>';
+    }
+}

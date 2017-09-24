a7d2cb4030f64a1ea1bca256b46c4bcc85f9f07d
chrischen86 <chrischen86@gmail.com>
2017-09-23 20:21:50 -0400

    Update service name

--- web/ConquestApi.php
+++ web/ConquestApi.php
@@ -24,7 +24,7 @@ $app->post('', function(Request $request){
     }
     else
     {
-        exec('/opt/php56/bin/php ' . dirname(__FILE__) . '/testReact.php > /dev/null &');
+        exec('/opt/php56/bin/php ' . dirname(__FILE__) . '/rtmClient.php > /dev/null &');
         return new Response("RTM deactivated, attempting to restart...", 200);
     }
     

1135221702d12a13c2810cc7b8a35fb5458dfd9d
chrischen86 <chrischen86@gmail.com>
2018-04-03 00:51:05 -0400

    Workaround to receive message

--- framework/slack/RealTimeClientExtended.php
+++ framework/slack/RealTimeClientExtended.php
@@ -1,14 +1,18 @@
 <?php
 
 namespace framework\slack;
+
 use Slack\RealTimeClient;
 use DateTime;
 
 class RealTimeClientExtended extends RealTimeClient
 {
+    private $eventSet = false;
+
     public function Ping()
     {
-        if (!$this->connected) {
+        if (!$this->connected)
+        {
             return \React\Promise\reject(new ConnectionException('Client not connected. Did you forget to call `connect()`?'));
         }
         $now = new DateTime();
@@ -17,6 +21,25 @@ class RealTimeClientExtended extends RealTimeClient
             'type' => 'ping',
             'time' => $now->format('Y-m-d H:i:s'),
         ];
+
+        if (!$this->eventSet)
+        {
+            $this->eventSet = true;
+            $this->websocket->on('message', function(\Devristo\Phpws\Messaging\WebSocketMessageInterface $message)
+            {
+                $payload = \Slack\Payload::fromJson($message->getData());
+                if (isset($payload['reply_to']))
+                {
+                    if (isset($this->pendingMessages[$payload['reply_to']]))
+                    {
+                        $deferred = $this->pendingMessages[$payload['reply_to']];
+                        $deferred->resolve();
+                        unset($this->pendingMessages[$payload['reply_to']]);
+                    }
+                }
+            });
+        }
+
         $this->websocket->send(json_encode($data));
         // Create a deferred object and add message to pending list so when a
         // success message arrives, we can de-queue it and resolve the promise.
@@ -24,4 +47,5 @@ class RealTimeClientExtended extends RealTimeClient
         $this->pendingMessages[$this->lastMessageId] = $deferred;
         return $deferred->promise();
     }
+
 }

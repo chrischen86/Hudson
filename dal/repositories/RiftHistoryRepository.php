0a949f2d99b90aea346008d2679961302383a805
chrischen86 <chrischen86@gmail.com>
2018-05-13 14:14:10 -0400

    Pull in slack message history in query as well

--- dal/repositories/RiftHistoryRepository.php
+++ dal/repositories/RiftHistoryRepository.php
@@ -65,10 +65,12 @@ class RiftHistoryRepository
         //and the create time (scheduled_time) is within the last hour
         $sql = 'SELECT h.id AS rift_history_id, h.owner_id, h.type_id, h.scheduled_time, h.is_deleted, h.slack_message_id, ' .
                 'u.id AS user_id, u.name, u.vip, u.is_archived, ' .
-                'r.id AS rift_type_id, r.name, r.thumbnail ' .
+                'r.id AS rift_type_id, r.name, r.thumbnail, ' .
+                's.id AS slack_message_history_id, s.ts AS slack_message_history_ts, s.channel as slack_message_history_channel ' .
                 'FROM rift_history h ' .
                 'INNER JOIN users u ON u.id = h.owner_id ' .
                 'LEFT JOIN rift_type r ON r.id = h.type_id ' .
+                'INNER JOIN slack_message_history s ON s.id = h.slack_message_id ' .
                 "WHERE owner_id = '" . $user->id . "' " .
                 "AND h.is_deleted = 0 " .
                 "AND h.scheduled_time > DATE_ADD(NOW(), INTERVAL -1 hour) " .

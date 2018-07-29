9c4ca97f6fdba53724121d0d90e24850fa295e12
chrischen86 <chrischen86@gmail.com>
2018-07-28 20:20:32 -0400

    Implement basic filtering of rift history

--- web/controllers/RiftController.php
+++ web/controllers/RiftController.php
@@ -6,6 +6,9 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use dal\repositories\RiftHistoryRepository;
 use dal\specifications\RiftHistoryByUserIdSpecification;
+use dal\SqlFilter;
+use dal\SqlParam;
+
 /**
  * Description of RiftController
  *
@@ -19,22 +22,39 @@ class RiftController
     private $repository;
 
     public function __construct(RiftHistoryRepository $repository)
-    {       
+    {
         $this->repository = $repository;
     }
-    
+
     public function getRiftHistory(Request $request, Application $app)
     {
         $params = $request->query->all();
+        $filter = $this->createFilterFromParameters($params);
+        $specification = new \dal\specifications\RiftHistoryByFilterSpecification($filter);
+        
+        $results = $this->repository->query($specification);
+        return $app->json($results);
+    }
+
+    private function createFilterFromParameters($params)
+    {
+        $filter = new SqlFilter();
         foreach ($params as $key => $val)
         {
-            error_log($key . ":" . $val);
+            switch ($key)
+            {
+                case "owner_id":
+                    $filter->addParam(new SqlParam($key, $val, SqlParam::$TEXT));
+                    break;
+                case "type_id":
+                    $filter->addParam(new SqlParam($key, $val, SqlParam::$NUMBER));
+                    break;
+                default:
+                    break;
+            }
         }
-        
-        $specification = new RiftHistoryByUserIdSpecification('U0KJBUYDC');
-        $results = $this->repository->query($specification);
 
-        return $app->json($results);
+        return $filter;
     }
 
 }
